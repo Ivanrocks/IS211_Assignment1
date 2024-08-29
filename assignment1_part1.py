14bdebb3892ef1bdeacea16079b14342549af944
kkk <ivanmartinez@ibm.com>
2024-08-29 18:57:34 -0400

    Ivan Martinez Week 1 Assignment

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,38 @@
-
-def list_divide(numbers, divide):
+def list_divide(numbers, divide = 2):
     """
-    The function returns the number of elements in the numbers list that are divisibleby divide
+        The function returns the number of elements in the numbers list that are divisibleby divide
+    """
+    result = 0
+    for number in numbers:
+        if number%divide == 0:
+            result+=1
+    return result
+
+class ListDivideException(Exception):
+    """
+    ListDivideException is a custom exception class that will be raised when a test case fails
     """
     pass
 
 def test_list_divide():
     """
-    Test listDivide
+        Testcases for listDivide.
+        If a test case fails there will be a Custom Exception raised
     """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
+    if list_divide([1,2,3,4,5]) != 2:
+        raise ListDivideException("Error: Test 1")
+
+    if list_divide([2,4,6,8,10]) != 5:
+        raise ListDivideException("Error: Test 2")
+
+    if list_divide([30,54,63,98,100],10) != 2:
+        raise ListDivideException("Error: Test 3")
+
+    if list_divide([]) != 0:
+        raise ListDivideException("Error: Test 4")
+
+    if list_divide([1,2,3,4,5], 1) != 5:
+        raise ListDivideException("Error: Test 5")
+
 if __name__ == "__main__":
-    testListDivide()
+    test_list_divide()
--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,24 @@
-
 class Book:
-    def __init__(self, author, title):
-        pass
-    
+    """
+    This class holds information about a book.
+
+    Attributes:
+        author: a string that contains the author of the book. Default is "".
+        title: a string that contains the title of the book. Default is "".
+    """
+    def __init__(self,author = "" , title = "" ):
+        """Initializes the attributes of the class"""
+        self.author = author
+        self.title = title
+
     def display(self):
-        pass
+        """Prints the phrase title, written by author"""
+        print(f"{self.title}, written by {self.author}")
+
 
 
 if __name__ == "__main__":
-    pass
+    Book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+    Book2 = Book("Walter Scott", "Ivanhoe: A Romance")
+    Book1.display()
+    Book2.display()
